510791aa0cb9d4c7ec37b95286d3669c2f2a5aae
kalnynsh <kda869@yandex.ru>
2021-07-07 14:03:35 +0300

    Init project with composer.

---
+++ public/index.php
@@ -0,0 +1,47 @@
+<?php
+
+use Framework\Http\Request;
+
+/** Go to Root dir */
+chdir(dirname(__DIR__));
+
+// require 'src/Framework/Http/Request.php';
+
+require 'vendor/autoload.php';
+
+session_start();
+
+
+### Initialization
+$request = new Request();
+
+$name = !empty($param = $request->getQueryParams()['name']) ? $param : 'Guest';
+$lang = getLang($request, 'en');
+
+header('X-developer: Denis');
+echo 'Hello, ' . $name . '! Are you speaking ' . $lang . ' ?';
+
+
+function getLang(
+    Request $request, 
+    string $default
+    ) {
+
+        if (!empty($lang = $request->getQueryParams()['lang'])) {
+            return $lang;
+        }
+
+        if (!empty($lang = $request->getCookies()['lang'])) {
+            return $lang;
+        }
+
+        if (!empty($lang = $request->getSession()['lang'])) {
+            return $lang;
+        }
+
+        if (!empty($serverAccpectLang = $request->getServer()['HTTP_ACCEPT_LANGUAGE'])) {
+            return substr($serverAccpectLang, 0, 2);
+        }
+
+        return $default;
+}
---
+++ src/Framework/Http/Request.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Framework\Http;
+
+class Request
+{
+    public function getQueryParams(): array
+    {
+        return $_GET;
+    }
+
+    /**
+     * getParsedBody - return parsed body or null.
+     * 
+     * TODO *
+     * For Json request: json decode body.
+     * For Xml request: SimpleXml decode body.
+     *
+     * @return array|null
+     */
+    public function getParsedBody(): array
+    {
+        return $_POST ?: null;
+    }
+
+    /**
+     * getBody - return body of HTTP request, like Json, xml
+     *
+     * @return string
+     */
+    public function getBody(): string
+    {
+        return file_get_contents('php://input');
+    }
+
+    public function getCookies(): array
+    {
+        return $_COOKIE;
+    }
+
+    public function getSession(): array
+    {
+        return $_SESSION;
+    }
+
+    public function getServer(): array
+    {
+        return $_SERVER;
+    }
+}
