56175175128a32a5bdb0236daa291f18ae98861f
kalnynsh <kda869@yandex.ru>
2021-07-08 17:14:05 +0300

    Implemented ResponseInterface in Response class.

--- src/Framework/Http/Response.php
+++ src/Framework/Http/Response.php
@@ -2,10 +2,13 @@
 
 namespace Framework\Http;
 
-class Response
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+
+class Response implements ResponseInterface
 {
     private array $headers = [];
-    private string $body;
+    private StreamInterface $body;
     private int $statusCode;
     private string $reasonPhrase = '';
 
@@ -20,22 +23,10 @@ class Response
 
     public function __construct(string $body = '', int $status = 200)
     {
-        $this->body = $body;
+        $streamBody = $body instanceof StreamInterface ?: new Stream($body);
+        $this->body = $streamBody;
         $this->statusCode = $status;
-    }
-
-    public function getBody(): string
-    {
-        return $this->body;
-    }
-
-    public function withBody(string $body): self
-    {
-        $new = clone $this;
-        $new->body = $body;
-
-        return $new;
-    }
+    }   
 
     public function getReasonPhrase(): string
     {
@@ -58,7 +49,7 @@ class Response
         return $this->statusCode;
     }
 
-    public function withStatus(int $code, string $reasonePhrase = ''): self
+    public function withStatus($code, $reasonePhrase = ''): self
     {
         $new = clone $this;
         $new->statusCode = $code;
@@ -72,12 +63,12 @@ class Response
         return $this->headers;
     }
 
-    public function hasHeader(string $headerName): bool
+    public function hasHeader($headerName)
     {
         return isset($this->headers[$headerName]);
     }
 
-    public function getHeader(string $headerName): string|null
+    public function getHeader($headerName)
     {
         if (!$this->hasHeader($headerName)) {
             return null;
@@ -86,9 +77,46 @@ class Response
         return $this->headers[$headerName];
     }
 
+    /**
+     * Retrieves a comma-separated string of the values for a single header.
+     *
+     * This method returns all of the header values of the given
+     * case-insensitive header name as a string concatenated together using
+     * a comma.
+     *
+     * NOTE: Not all header values may be appropriately represented using
+     * comma concatenation. For such headers, use getHeader() instead
+     * and supply your own delimiter when concatenating.
+     *
+     * If the header does not appear in the message, this method MUST return
+     * an empty string.
+     *
+     * @param string $name Case-insensitive header field name.
+     * @return string A string of values as provided for the given header
+     *    concatenated together using a comma. If the header does not appear in
+     *    the message, this method MUST return an empty string.
+     */
+    public function getHeaderLine($name)
+    {}
+
+    /**
+     * Return an instance with the provided value replacing the specified header.
+     *
+     * While header names are case-insensitive, the casing of the header will
+     * be preserved by this function, and returned from getHeaders().
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that has the
+     * new and/or updated header and value.
+     *
+     * @param string $name Case-insensitive header field name.
+     * @param string|string[] $value Header value(s).
+     * @return static
+     * @throws \InvalidArgumentException for invalid header names or values.
+     */
     public function withHeader(
-        string $headerName, 
-        string $headerValue
+        $headerName, 
+        $headerValues
     ): self {
         $new = clone $this;
 
@@ -96,7 +124,113 @@ class Response
             unset($new->headers[$headerName]);
         }
 
-        $new->headers[$headerName] = $headerValue;
+        $new->headers[$headerName] = (array)$headerValues;
+
+        return $new;
+    }
+
+    /**
+     * Return an instance with the specified header appended with the given value.
+     *
+     * Existing values for the specified header will be maintained. The new
+     * value(s) will be appended to the existing list. If the header did not
+     * exist previously, it will be added.
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that has the
+     * new header and/or value.
+     *
+     * @param string $name Case-insensitive header field name to add.
+     * @param string|string[] $value Header value(s).
+     * @return static
+     * @throws \InvalidArgumentException for invalid header names or values.
+     */
+    public function withAddedHeader($name, $value): self
+    {
+        if ($this->hasHeader($name)) {
+            $new = clone $this;
+            $new->headers[$name] = array_merge($new->headers[$name], (array) $value);
+
+            return $new;
+        }
+    }
+
+    /**
+     * Return an instance without the specified header.
+     *
+     * Header resolution MUST be done without case-sensitivity.
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that removes
+     * the named header.
+     *
+     * @param string $name Case-insensitive header field name to remove.
+     * @return static
+     */
+    public function withoutHeader($name): self
+    {
+        $new = clone $this;
+
+        if ($new->hasHeader($name)) {
+            unset($new->headers[$name]);
+        }
+
+        return $new;
+    }
+
+    /**
+     * Retrieves the HTTP protocol version as a string.
+     *
+     * The string MUST contain only the HTTP version number (e.g., "1.1", "1.0").
+     *
+     * @return string HTTP protocol version.
+     */
+    public function getProtocolVersion()
+    {}
+
+    /**
+     * Return an instance with the specified HTTP protocol version.
+     *
+     * The version string MUST contain only the HTTP version number (e.g.,
+     * "1.1", "1.0").
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that has the
+     * new protocol version.
+     *
+     * @param string $version HTTP protocol version
+     * @return static
+     */
+    public function withProtocolVersion($version)
+    {}
+
+    /**
+     * Gets the body of the message.
+     *
+     * @return StreamInterface Returns the body as a stream.
+     */
+    public function getBody(): StreamInterface
+    {
+        return $this->body;
+    }
+
+    /**
+     * Return an instance with the specified message body.
+     *
+     * The body MUST be a StreamInterface object.
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return a new instance that has the
+     * new body stream.
+     *
+     * @param StreamInterface $body Body.
+     * @return static
+     * @throws \InvalidArgumentException When the body is not valid.
+     */
+    public function withBody(StreamInterface $body): self
+    {
+        $new = clone $this;
+        $new->body = $body;
 
         return $new;
     }
