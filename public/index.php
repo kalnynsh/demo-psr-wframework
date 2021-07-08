b79b63586726997119b88af581a85ac938eec20b
kalnynsh <kda869@yandex.ru>
2021-07-08 13:14:08 +0300

    Used Response class.

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
 
 use Framework\Http\RequestFactory;
+use Framework\Http\Response;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
 session_start();
-header('X-Developer: Denis');
 
 ### Initialization
 $request = RequestFactory::fromGlobals();
@@ -14,5 +14,21 @@ $request = RequestFactory::fromGlobals();
 ### Action
 $queryParams = $request->getQueryParams();
 $name = $queryParams['name'] ?? 'Guest';
+$body = 'Hello, ' . $name . '!';
 
-echo 'Hello, ' . $name . '!';
+$response = (new Response())
+    ->withBody($name)
+    ->withHeader('X-Developer', 'Denis');
+
+header(
+    'HTTP/1.1 '
+    . $response->getStatusCode() 
+    . ' ' 
+    . $response->getReasonPhrase()
+);
+
+foreach ($response->getHeaders() as $name => $value) {
+    header($name . ': ' . $value);
+}
+
+echo $response->getBody();
