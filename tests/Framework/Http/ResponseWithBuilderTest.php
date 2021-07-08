95ea88f849947ef1fa329eff76c22e1b8c3c1117
kalnynsh <kda869@yandex.ru>
2021-07-08 15:20:55 +0300

    Split response tests on 2 classes.

--- tests/Framework/Http/ResponseWithBuilderTest.php
+++ tests/Framework/Http/ResponseWithBuilderTest.php
@@ -2,15 +2,16 @@
 
 namespace Test\Framework\Http;
 
-use Framework\Http\Response;
 use PHPUnit\Framework\TestCase;
 
-class ResponseTest extends TestCase
+class ResponseWithBuilderTest extends TestCase
 {
     public function testEmpty(): void
     {
-        $response = new Response($body = 'HTTP BODY');
- 
+        $response = (new ResponseBuilder)
+            ->withBody($body = 'HTTP BODY')
+            ->build();
+
         self::assertEquals($body, $response->getBody());
         self::assertEquals(200, $response->getStatusCode());
         self::assertEquals('OK', $response->getReasonPhrase());
@@ -18,19 +19,23 @@ class ResponseTest extends TestCase
 
     public function testStatus404(): void
     {
-        $response = new Response($body = 'HTTP BODY', $statusCode = 404);
- 
+        $response = (new ResponseBuilder)
+            ->withBody($body = 'HTTP BODY')
+            ->withStatus($statusCode = 404)
+            ->build();
+
         self::assertEquals($body, $response->getBody());
         self::assertEquals($statusCode, $response->getStatusCode());
         self::assertEquals('Not Found', $response->getReasonPhrase());
     }
 
     public function testHeaders(): void
-    {       
-        $response = (new Response($body = 'HTTP BODY'))
+    {
+        $response = (new ResponseBuilder)
+            ->withBody($body = 'HTTP BODY')
             ->withHeader($headerName1 = 'X-Name-1', $headerValue1 = 'Value_1')
-            ->withHeader($headerName2 = 'X-Name-2', $headerValue2 = 'Value_2');
-         
+            ->withHeader($headerName2 = 'X-Name-2', $headerValue2 = 'Value_2')
+            ->build();
 
         self::assertEquals([
             $headerName1 => $headerValue1,
