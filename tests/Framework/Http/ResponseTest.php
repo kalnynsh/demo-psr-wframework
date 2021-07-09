948277e638a656c7d20f6a13f589c668afbe0b2d
kalnynsh <kda869@yandex.ru>
2021-07-09 12:46:41 +0300

    Updated ResponseTest with Laminas Response.

--- tests/Framework/Http/ResponseTest.php
+++ tests/Framework/Http/ResponseTest.php
@@ -2,26 +2,29 @@
 
 namespace Test\Framework\Http;
 
-use Framework\Http\Response;
+use Laminas\Diactoros\Response;
+use Laminas\Diactoros\StreamFactory;
 use PHPUnit\Framework\TestCase;
 
 class ResponseTest extends TestCase
 {
     public function testEmpty(): void
     {
-        $response = new Response($body = 'HTTP BODY');
+        $stream = (new StreamFactory)->createStream($content = 'HTTP content');
+        $response = new Response($stream);
  
-        self::assertEquals($body, $response->getBody()->getContents());
+        self::assertEquals($content, $response->getBody()->getContents());
         self::assertEquals(200, $response->getStatusCode());
         self::assertEquals('OK', $response->getReasonPhrase());
     }
 
     public function testStatus404(): void
     {
-        $response = new Response($body = 'HTTP BODY', $statusCode = 404);
+        $stream = (new StreamFactory)->createStream($content = 'HTTP content');
+        $response = new Response($stream, $statusCode = 404);
  
-        self::assertEquals($body, $response->getBody()->getContents());
-        self::assertEquals(mb_strlen($body), $response->getBody()->getSize());
+        self::assertEquals($content, $response->getBody()->getContents());
+        self::assertEquals(mb_strlen($content), $response->getBody()->getSize());
 
         self::assertEquals($statusCode, $response->getStatusCode());
         self::assertEquals('Not Found', $response->getReasonPhrase());
