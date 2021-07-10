aa51609105a8cd519c5a1e8742d1e2e49fbf3877
kalnynsh <kda869@yandex.ru>
2021-07-10 16:09:52 +0300

    Added simple IF routing.

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use Laminas\Diactoros\ServerRequestFactory;
 use Laminas\Diactoros\Response\HtmlResponse;
+use Laminas\Diactoros\Response\JsonResponse;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 
 chdir(dirname(__DIR__));
@@ -19,12 +20,43 @@ $request = ServerRequestFactory::fromGlobals(
 );
 
 ### Action
-$queryParams = $request->getQueryParams();
-$name = $queryParams['name'] ?? 'Guest';
-$content = 'Hello, ' . $name . '!';
+$path = $request->getUri()->getPath();
 
-$response = (new HtmlResponse($content))
-    ->withHeader('X-Developer', 'Denis');
+if ($path === '/') {
+    $queryParams = $request->getQueryParams();
+    $name = $queryParams['name'] ?? 'Guest';
+    $content = 'Hello, ' . $name . '!';
+
+    $response = new HtmlResponse($content);
+} elseif ($path === '/about') {
+    $response = new HtmlResponse('I am very simple site;)');
+} elseif ($path === '/blog') {
+    $response = new JsonResponse([
+        [
+            'id' => 2, 'title' => 'The second post',
+        ],
+        [
+            'id' => 1, 'title' => 'The first post',
+        ]
+    ]);
+} elseif(preg_match('#^/blog/(?P<id>\d+)#i', $path, $matches)) {
+    $id = $matches['id'];
+    $maxPastsCount = 2;
+
+    if ($id > $maxPastsCount) {
+        $response = new JsonResponse(['error' => 'Undefined page'], 404);
+    }
+
+    if ($id <= $maxPastsCount) {
+        $response = new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
+    }
+} else  {
+    $response = new HtmlResponse('Undefined page', 404);
+}
+
+
+## Postprosessing
+$response = $response->withHeader('X-Developer', 'Denis');
 
 $emmiter = new SapiEmitter();
 $emmiter->emit($response);
