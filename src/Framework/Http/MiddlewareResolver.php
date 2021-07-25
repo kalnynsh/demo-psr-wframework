d216c36b643e33b4782757184597119f6cbb9326
kalnynsh <kda869@yandex.ru>
2021-07-25 15:08:57 +0300

    Moved functionality check on array and create Pipeline to MiddlewareResolver.

--- src/Framework/Http/MiddlewareResolver.php
+++ src/Framework/Http/MiddlewareResolver.php
@@ -2,12 +2,17 @@
 
 namespace Framework\Http;
 
+use Framework\Http\Pipeline\Pipeline;
 use Psr\Http\Message\ServerRequestInterface;
 
 class MiddlewareResolver
 {
     public function resolve($handler): callable
     {
+        if (\is_array($handler)) {
+            return $this->createPipe($handler);
+        }
+
         if (\is_string($handler)) {
             
             return function (ServerRequestInterface $request, callable $next) use ($handler) {
@@ -17,7 +22,19 @@ class MiddlewareResolver
             };
         }
 
-        /** $handler not a string */
+        /** $handler not a string and not array */
         return $handler;
+    }
+
+    private function createPipe(array $handlers): Pipeline 
+    {
+        $pipeline = new Pipeline();
+
+        foreach ($handlers as $handler)  {
+            $pipeline->pipe($this->resolve($handler));
+        }
+
+
+        return $pipeline;
     }
 }
