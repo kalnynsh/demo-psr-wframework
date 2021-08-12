19117bdadf778cdf9f93d017a69d93f62b0ce637
kalnynsh <kda869@yandex.ru>
2021-08-12 17:37:32 +0300

    Added var types in doc string.

--- src/Framework/Http/Router/Result.php
+++ src/Framework/Http/Router/Result.php
@@ -2,39 +2,54 @@
 
 namespace Framework\Http\Router;
 
+use Psr\Http\Server\RequestHandlerInterface;
+
 class Result
 {
     private string $name;
-    private $handler;
+
+    /** @var class-string<RequestHandlerInterface> */
+    private string $handler;
     private array $attributes;
 
-    public function __construct(string $name, $handler, array $attributes)
-    {
+    public function __construct(
+        string $name, 
+        string $handler, 
+        array $attributes
+    ) {
         $this->name = $name;
+
+        /** @var class-string<RequestHandlerInterface> $handler */
         $this->handler = $handler;
         $this->attributes = $attributes;
     }
 
     /**
      * Get the value of name
+     * 
+     * @return string
      */ 
-    public function getName()
+    public function getName(): string
     {
         return $this->name;
     }
 
     /**
      * Get the value of handler
+     * 
+     * @return class-string<RequestHandlerInterface>
      */ 
-    public function getHandler()
+    public function getHandler(): string
     {
         return $this->handler;
     }
 
     /**
      * Get the value of attributes
+     * 
+     * @return array<non-empty-string, mixed>
      */ 
-    public function getAttributes()
+    public function getAttributes(): array
     {
         return $this->attributes;
     }
