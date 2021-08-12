9414de3e909fa008eb7e0df3b084f96a414ed732
kalnynsh <kda869@yandex.ru>
2021-08-12 18:13:21 +0300

    Used Laminas\Stratigility components.

--- public/index.php
+++ public/index.php
@@ -3,12 +3,16 @@
 use App\Http\Action;
 use App\Http\Middleware;
 use Framework\Http\Application;
+use Laminas\Diactoros\Response;
 use Aura\Router\RouterContainer;
+use function Laminas\Stratigility\path;
 use Laminas\Diactoros\ServerRequestFactory;
 use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Pipeline\MiddlewareResolver;
-use Laminas\Diactoros\Response;
+use Laminas\Stratigility\Middleware\ErrorHandler;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use Laminas\Stratigility\Middleware\NotFoundHandler;
+use Laminas\Stratigility\Middleware\ErrorResponseGenerator;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
@@ -35,12 +39,8 @@ $routes->get('about', '/about', Action\Home\AboutAction::class);
 
 $routes->get(
     'cabinet', 
-    '/cabinet',
-    [
-        Middleware\ProfilerMiddleware::class,
-        new Middleware\BasicAuthMiddleware($usersParams['users']),
-        Action\Home\CabinetAction::class,
-    ]    
+    '/cabinet',      
+    Action\Home\CabinetAction::class 
 );
 
 $routes->get('blog', '/blog', Action\Blog\IndexAction::class);
@@ -49,25 +49,46 @@ $routes->get(
     'blog_show',
     '/blog/{id}',
     Action\Blog\ShowAction::class    
-)->tokens(['id' => '\d+']);
+)->tokens(['id' => '\\d+']);
 
 $router = new AuraRouterAdapter($auraRouterContainer);
 $resolver = new MiddlewareResolver();
-$app = new Application($resolver, new Middleware\NotFoundHandler());
+$app = new Application($resolver);
 
-$app->pipe(new Middleware\ErrorHandlerMiddleware($params['debug']));
+// Laminas ErrorHandler
+$app->pipe(
+    new ErrorHandler(
+        function () {
+            return new Response();
+        }, 
+        new ErrorResponseGenerator($params['debug'])
+    )
+);
 
-/** Global ProfileMiddleware */
+// Own ErrorHandlerMiddleware
+// $app->pipe(new Middleware\ErrorHandlerMiddleware($params['debug']));
+
+/* Global ProfileMiddleware */
 $app->pipe(Middleware\ProfilerMiddleware::class);
 $app->pipe(Middleware\CredentialsMiddleware::class);
-$app->pipe(new \Framework\Http\Middleware\RouteMiddleware($router));
-$app->pipe(new \Framework\Http\Middleware\DispatcherMiddleware($resolver));
+
+$app->pipe(new Framework\Http\Middleware\RouteMiddleware($router));
+
+$app->pipe(path(
+    '/cabinet', 
+    new Middleware\BasicAuthMiddleware($usersParams['users'])
+));
+
+$app->pipe(new Framework\Http\Middleware\DispatcherMiddleware());
+
+$app->pipe(new NotFoundHandler(function () {
+    return new Response();
+}));
 
 # Running
 $request = ServerRequestFactory::fromGlobals();
-$response = $app->run($request, new Response());
+$response = $app->run($request);
 
 ## Sending
-
 $emmiter = new SapiEmitter();
 $emmiter->emit($response);
