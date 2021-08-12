6e08046e53b4d43754fe4324c273b1a8c089737c
kalnynsh <kda869@yandex.ru>
2021-08-12 18:08:54 +0300

    Used only MiddlewareInterface in $handler.

--- src/Framework/Http/Pipeline/MiddlewareResolver.php
+++ src/Framework/Http/Pipeline/MiddlewareResolver.php
@@ -2,77 +2,60 @@
 
 namespace Framework\Http\Pipeline;
 
-use Psr\Http\Message\ResponseInterface;
+use Laminas\Stratigility\MiddlewarePipe;
 use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Framework\Http\Middleware\Exception\UnknownMiddlewareTypeException;
 
 class MiddlewareResolver
 {
-    public function resolve($handler): callable
+    /**
+     * Resolve given parameter $handler
+     * 
+     * @template T of MiddlewareInterface
+     * @template TValue string|T
+     * 
+     * @param string|array<positive-int, TValue>|T $handler
+     * @throws UnknownMiddlewareTypeException
+     * 
+     * @return T
+     */
+    public function resolve($handler): MiddlewareInterface
     {
         if (\is_array($handler)) {
             return $this->createPipe($handler);
         }
 
+        /** @var class-string<MiddlewareInterface> $handler */
         if (\is_string($handler)) {
-            
-            return function (
-                ServerRequestInterface $request, 
-                ResponseInterface $response, 
-                callable $next
-            ) use ($handler) {
-                $middleware = $this->resolve(new $handler());
 
-                return $middleware($request, $response, $next);
-            };
+            if (! class_exists($handler)) {
+                throw new UnknownMiddlewareTypeException($handler);
+            }
+
+            /** @var MiddlewareInterface */
+            $middleware = new $handler();
+
+            if ($middleware instanceof MiddlewareInterface) {
+                return $middleware;
+            }            
         }
 
         if ($handler instanceof MiddlewareInterface) {
-            return function(
-                ServerRequestInterface $request,
-                ResponseInterface $response,
-                callable $next
-            ) use ($handler) {
-                return $handler->process(
-                    $request, 
-                    new RequestHandlerWrapper($next)
-                );
-            };
-        }
-
-        if (\is_object($handler)) {
-            $reflection = new \ReflectionObject($handler);
-
-            if ($reflection->hasMethod('__invoke')) {
-                $method = $reflection->getMethod('__invoke');
-                $parameters = $method->getParameters();
-
-                if (\count($parameters) === 2 && $parameters[1]->isCallable()) {
-                    return function (
-                        ServerRequestInterface $request, 
-                        ResponseInterface $response, 
-                        callable $next
-                    ) use ($handler) {
-                        return $handler($request, $next);
-                    };
-                }
-            }
-
             return $handler;
         }
 
-        throw new UnknownMiddlewareTypeException($handler);
+        throw new UnknownMiddlewareTypeException(\gettype($handler));
     }
 
-    private function createPipe(array $handlers): Pipeline 
+
+    private function createPipe(array $middlewares): MiddlewarePipe
     {
-        $pipeline = new Pipeline();
+        $pipeline = new MiddlewarePipe();
 
-        foreach ($handlers as $handler)  {
-            $pipeline->pipe($this->resolve($handler));
+        /** @var class-string<MiddlewareInterface>|MiddlewareInterface $middleware */
+        foreach ($middlewares as $middleware) {
+            $pipeline->pipe($this->resolve($middleware));
         }
-
 
         return $pipeline;
     }
