52bad95a894a0a01f22d365daa3f8954e7f72d11
kalnynsh <kda869@yandex.ru>
2021-08-13 18:33:42 +0300

    Created test for class Container.

---
+++ tests/Framework/Container/ContainerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Test\Framework\Container;
+
+use Framework\Container;
+use Framework\Container\Exeption\ServiceNotFoundException;
+use PHPUnit\Framework\TestCase;
+
+class ContainerTest extends TestCase
+{
+    public $backupStaticAttributes = false;
+    public $runTestInSeparateProcess = true;
+
+    public function testPrimitives(): void
+    {
+        $container = new Container();
+
+        $container->set($name = 'Number_service', $value = 5);
+        self::assertEquals($value, $container->get($name));
+
+        $container->set($name = 'String_service', $value = 'Good');
+        self::assertEquals($value, $container->get($name));
+
+        $container->set($name = 'Array_service', $value = ['Good', 'Better']);
+        self::assertEquals($value, $container->get($name));
+
+        $container->set($name = 'Object_service', $value = new \stdClass());
+        self::assertEquals($value, $container->get($name));
+    }
+
+    public function testNotFound()
+    {
+        $container = new Container();
+
+        $this->expectException(ServiceNotFoundException::class);
+
+        $container->get('Email_service');
+    }
+}
