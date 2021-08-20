6bb5553a11a297604b254e7c2315dfe5e825b915
kalnynsh <kda869@yandex.ru>
2021-08-20 20:47:35 +0300

    Added Laminas ConfigAggregator.

--- config/config.php
+++ config/config.php
@@ -1,6 +1,9 @@
 <?php
 
-defined ('DIR_PREFFIX') || define(
+use Laminas\ConfigAggregator\ConfigAggregator;
+use Laminas\ConfigAggregator\PhpFileProvider;
+
+defined ('DIR_PREFFIX') || define (
         'DIR_PREFFIX',
         __DIR__
         . DIRECTORY_SEPARATOR
@@ -8,11 +11,8 @@ defined ('DIR_PREFFIX') || define(
         . DIRECTORY_SEPARATOR
 );
 
-$configs = array_map (
-    function ($file) {
-        return require $file;
-    },
-    glob (DIR_PREFFIX . '{{,*.}global,{,*.}local}.php', GLOB_BRACE)
-);
+$aggregator = new ConfigAggregator([
+    new PhpFileProvider(DIR_PREFFIX . '{{,*.}global,{,*.}local}.php'),
+]);
 
-return \array_merge_recursive (...$configs);
+return $aggregator->getMergedConfig();
