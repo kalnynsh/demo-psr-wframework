5500f519f71fbe5b993bd62224ac26ee5f6017a2
kalnynsh <kda869@yandex.ru>
2021-08-20 17:15:14 +0300

    Removed not used require.

--- config/config.php
+++ config/config.php
@@ -8,8 +8,6 @@ defined ('DIR_PREFFIX') || define(
         . DIRECTORY_SEPARATOR
 );
 
-require __DIR__ . DIRECTORY_SEPARATOR . 'array_merge_recursive_distinct.php';
-
 return \array_merge_recursive (
     require DIR_PREFFIX . 'app.global.php',
     require DIR_PREFFIX . 'auth.global.php',
