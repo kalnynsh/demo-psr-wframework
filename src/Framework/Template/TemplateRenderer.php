5b9901acccef5e3fe16f5b8d9fb5ce15348268b6
kalnynsh <kda869@yandex.ru>
2021-08-29 16:31:14 +0300

    Updated TemplateRenderer::render.

--- src/Framework/Template/TemplateRenderer.php
+++ src/Framework/Template/TemplateRenderer.php
@@ -11,13 +11,27 @@ class TemplateRenderer
         $this->path = $path;
     }
 
+    /**
+     * Render given view
+     *
+     * @param string $view - view name
+     * @param array $params - extracting to view
+     * 
+     * @throws VewFileNotExists
+     * @return string|null
+     */
     public function render(string $view, array $params): ?string
     {
         /** @var string $templateFile */
         $templateFile = $this->path . DIRECTORY_SEPARATOR . $view . '.php';
 
+        if (! file_exists($templateFile)) {
+            throw new VewFileNotExists();
+        } 
+
         ob_start();
         extract($params, EXTR_OVERWRITE);
+       
         require $templateFile;
 
         return ob_get_clean();
