d099eba1534a4d2fe311a02d55097fcad19177ec
kalnynsh <kda869@yandex.ru>
2021-09-19 20:36:06 +0300

    Created LazyMiddlewareDecorator.

---
+++ src/Framework/Http/Pipeline/LazyMiddlewareDecorator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Framework\Http\Pipeline;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Framework\Http\Pipeline\MiddlewareResolver;
+
+class LazyMiddlewareDecorator implements MiddlewareInterface
+{
+    private $resolver;
+    private $container;
+    private $service;
+
+    public function __construct(
+        MiddlewareResolver $resolver,
+        ContainerInterface $container,
+        string $service
+    ) {
+        $this->resolver = $resolver;
+        $this->container = $container;
+        $this->service = $service;
+    }
+
+    public function process(
+        ServerRequestInterface $request,
+        RequestHandlerInterface $handler
+    ): ResponseInterface {
+        /** @var MiddlewareInterface */
+        $middleware = $this
+            ->resolver
+            ->resolve($this->container->get($this->service));
+
+        return $middleware->process($request, $handler);
+    }
+}
