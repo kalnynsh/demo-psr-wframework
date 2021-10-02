812df811d50519dc5118d57f28b602820a37bf16
kalnynsh <kda869@yandex.ru>
2021-10-02 20:48:47 +0300

    Added favicon.

--- templates/layout/main.php
+++ templates/layout/main.php
@@ -11,6 +11,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
       rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
       crossorigin="anonymous" />
+    <link href="/images/house-237998/favicon.ico"
+      rel="shortcut icon" type="image/ico"  />
     <title>
       <?= $this->blockRender('title'); ?>
     </title>
