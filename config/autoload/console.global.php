b74c147e1a0675a5f8ca0ee0dbbb14849a2c100c
kalnynsh <kda869@yandex.ru>
2021-10-16 17:24:50 +0300

    Added cache console command config.

---
+++ config/autoload/console.global.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Console\Command;
+
+return [
+    'dependencies' => [
+        'factories' => [
+            Command\CacheClearCommand::class
+                => \Infrastructure\App\Console\Command\CacheClearCommandFactory::class,
+        ],
+    ],
+    'console' => [
+        'cachePaths' => [
+            'twig' => 'var/cache/twig',
+        ],
+    ],
+];
---
+++ src/Infrastructure/App/Console/Command/CacheClearCommandFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Infrastructure\App\Console\Command;
+
+use App\Console\Command\CacheClearCommand;
+use Psr\Container\ContainerInterface;
+
+class CacheClearCommandFactory
+{
+    public function __invoke(
+        ContainerInterface $container,
+        string $requestedName,
+        ?array $options = null
+    ) {
+        return new CacheClearCommand(
+            $container->get('config')['console']['cachePaths']
+        );
+    }
+}
